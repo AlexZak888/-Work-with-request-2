2448ee9cfa515e66461cd4efe2f21559274c5b4b
Aleksandr Zakharov <laew2012@mail.ru>
2023-02-21 20:22:18 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from settings import TOKEN
+
+
+class YaUploader:
+
+    def __init__(self, token: str):
+        self.token = token
+        self.HOST = 'https://cloud-api.yandex.net:443'
+        self.headers = {'Authorization': f'OAuth {self.token}', 'Content-Type': 'application/json'}
+
+    def upload(self, path_to_file):
+        uri = 'v1/disk/resources/upload'
+        url = self.HOST + uri
+        file_name = path_to_file.split("\\")[-1]
+        params = {'path': file_name, 'overwrite': 'true'}
+        response = requests.get(url, headers=self.get_headers(), params=params)
+        upload_link = response.json()['href']
+        print(upload_link)
+        resp = requests.put(upload_link, headers=self.get_headers(), data=open(path_to_file, 'rb'))
+        if resp.status_code == 201 or 203:
+            print('Success')
+        else:
+            (print(f' Код ошибки {resp.status_code}'))
+
+
+if __name__ == '__main__':
+
+    path_to_file = ...
+    token = ...
+    uploader = YaUploader(TOKEN)
+    result = uploader.upload(path_to_file)
